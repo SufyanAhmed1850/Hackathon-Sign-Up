89f8c0be882b9e7fb0c23699ba72b11e49b36800
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-08-19 19:51:42 +0500

    b

--- Profile/app.js
+++ Profile/app.js
@@ -1,4 +1,19 @@
-import app from "../config";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCtPWyKIRMJsfWsLmwwlsArGw9788eQ8Sg",
+    authDomain: "hackathonsmit-ba16c.firebaseapp.com",
+    projectId: "hackathonsmit-ba16c",
+    storageBucket: "hackathonsmit-ba16c.appspot.com",
+    messagingSenderId: "420550745532",
+    appId: "1:420550745532:web:772da6c2eb1e6442191fb9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 import {
     getStorage,
     ref,
